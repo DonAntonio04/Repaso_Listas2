e466320ad3d9ac53ea8cb67131a09c3773bdcdde
DonAntonio04 <berrellezantonio04@gmail.com>
2023-11-18 19:31:58 -0700

    nuevo

---
+++ Repaso_Listas/Listas.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso_Listas
+{
+    public class Listas
+    {
+        Nodo primero;
+        Nodo ultimo;
+
+        public bool ListaVacia()
+        {
+            if (primero == null)
+            {
+                return true;
+            }
+            return false;
+
+        }
+       public int LongitudLista()
+        {
+            int contador = 0;
+            if(primero == null)
+            {
+                return contador;
+            }
+            else
+            {
+                if(primero != null && primero.getSiguiente != null)
+                {
+                    return (contador + 1);
+                }
+                else
+                {
+
+                }
+            }
+        }
+     }
+}
---
+++ Repaso_Listas/Nodo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso_Listas
+{
+     public class Nodo
+     {
+        private int _datos;
+        private Nodo _Siguiente;
+
+        public Nodo(int valor)
+        {
+            _datos = valor;
+            _Siguiente = null;
+
+        }
+        public Nodo(int valor, Nodo siguiente)
+        {
+            _datos =valor;
+            _Siguiente = siguiente;
+        }
+        public int getDatos()
+        {
+            return _datos;
+        }
+        public Nodo getSiguiente()
+        {
+            return _Siguiente;
+        }
+     }
+}
